e9943521ab3856c7f8f6081c8a071139ddac69d2
sukumaranshravan <shravansukumaran@yahoo.in>
2024-03-31 20:24:00 +0530

    desnag

--- public/styles/my.js
+++ public/styles/my.js
@@ -2,19 +2,19 @@ const day = ["dawn","morning","noon","evening","dusk","night"]
 
 let curTime = document.querySelector("#hour").innerHTML
 
-if(curTime>20){
+if(curTime>19){
     let src = "/images/day/"+day[5]+".jpg"
     document.querySelector("#top-box").setAttribute("style",`background-image: url(${src})`)
-}else if(curTime>18){
+}else if(curTime>17){
     let src = "/images/day/"+day[4]+".jpg"
     document.querySelector("#top-box").setAttribute("style",`background-image: url(${src})`)
-}else if(curTime>=14){
+}else if(curTime>=13){
     let src = "/images/day/"+day[3]+".jpg"
     document.querySelector("#top-box").setAttribute("style",`background-image: url(${src})`)
-}else if(curTime>12){
+}else if(curTime>11){
     let src = "/images/day/"+day[2]+".jpg"
     document.querySelector("#top-box").setAttribute("style",`background-image: url(${src})`)
-}else if(curTime>7){
+}else if(curTime>6){
     let src = "/images/day/"+day[1]+".jpg"
     document.querySelector("#top-box").setAttribute("style",`background-image: url(${src})`)
 }else if(curTime>4){
